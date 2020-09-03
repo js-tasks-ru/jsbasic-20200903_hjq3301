38238461d5cfe09dc6e909b5ecea27e7998f849c
dmi3301 <hjq3301@mail.ru>
2020-09-03 16:19:26 +0700

    return task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,15 @@
  * @returns {number}
  */
 function factorial(n) {
-  // ваш код...
-}
+  let i = 1;
+  while(n) {
+    i *= n--;
+  }
+  return i;
+}  
+factorial(0); 
+factorial(1);
+factorial(3); 
+factorial(5); 
+
+
