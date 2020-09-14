4086307778f9330ce274f443b31f3aed1002f627
dmitriew <hjq3301@mail.ru>
2020-09-14 15:48:40 +0700

    return 3-task

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,16 @@
-let calculator = {
-  // ваш код
+let calculator  = {
+  a : 0, //ключ и значение
+  b : 0,
+  read(a,b) {  
+      this.a = a; //сохр.
+      this.b = b;
+  },
+  sum() {
+      return this.a+this.b;
+  },
+  mul() {
+      return this.a*this.b 
+  },
 };
-
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
