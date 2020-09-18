f419e0e8808899ccbafe64f3edd7f8bea753496d
dmitriew <hjq3301@mail.ru>
2020-09-18 14:44:41 +0700

    return 3-3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,13 +3,13 @@
  * @returns {string}
  */
 function camelize(str) {
- let i = str.split('-');
- let b = i.map((word,index) {
-   if (index == 0) {
-     return false
-   } else {
-     word[0].toUpperCase() + word.slice(1);
-     return i.join('');
-   }
- })
+ let i = str.split('-').map(function(word,index) {
+  if (index == 0) {
+    return word
+  } else {
+    let c = word[0].toUpperCase() + word.slice(1);
+    return c;
+  }
+}) 
+ return i.join('');
 }
