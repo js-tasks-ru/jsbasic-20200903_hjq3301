8e067e99e77b1fce71ce16a8b5285c24a16782f9
dmitriew <hjq3301@mail.ru>
2020-10-11 14:47:36 +0700

    feat: task 5-3

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,23 @@
 function initCarousel() {
-  // ваш код...
+  const right = document.querySelector('.carousel__arrow_right');
+  const left = document.querySelector('.carousel__arrow_left');
+  const carousel = document.querySelector('.carousel__inner')
+  let computedStyle = getComputedStyle(carousel)
+  position = 0;
+  left.style.display = 'none';
+
+  
+
+  right.addEventListener( "click" , function() {
+      left.style.display = 'flex';
+      carousel.style.transform = 'translateX(-988px)'
+      });
+
+
+  left.addEventListener( "click" , function() {
+    carousel.style.transform = 'translateX(0)'
+    left.style.display = 'none';
+    console.log(computedStyle.transform);
+  });
+
 }
